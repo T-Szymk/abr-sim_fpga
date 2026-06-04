68fd75e6d86cef83767c2e64acd6307e4dcdf670
Tom Szymkowiak <phys2928@ox.ac.uk>
2026-06-04 15:37:26 +0300

    implement memory array

--- fpga/rtl/abr_ram_array.sv
+++ fpga/rtl/abr_ram_array.sv
@@ -1,27 +1,7 @@
 // abr_ram_array.sv - RAM array module to allow byte-writes on A-ports and 
 // word-reads on B-ports of true dual-port BRAMs. This is necessary to support 
 // byte accesses of USB side and 32b accesses on ABR side. CDC also occurs over 
-// the memory interface, so this module also serves as a convenient boundary 
-// for synchronizers and CDC verification.
-
-// <wire_or_reg> [clogb2(RAM_DEPTH-1)-1:0] <addra>;  // Port A address bus, width determined from RAM_DEPTH
-//   <wire_or_reg> [clogb2(RAM_DEPTH-1)-1:0] <addrb>;  // Port B address bus, width determined from RAM_DEPTH
-//   <wire_or_reg> [(NB_COL*COL_WIDTH)-1:0] <dina>;  // Port A RAM input data
-//   <wire_or_reg> [(NB_COL*COL_WIDTH)-1:0] <dinb>;  // Port B RAM input data
-//   <wire_or_reg> <clka>;                           // Port A clock
-//   <wire_or_reg> <clkb>;                           // Port B clock
-//   <wire_or_reg> [NB_COL-1:0] <wea>;               // Port A write enable
-//   <wire_or_reg> [NB_COL-1:0] <web>;		  // Port B write enable
-//   <wire_or_reg> <ena>;                            // Port A RAM Enable, for additional power savings, disable BRAM when not in use
-//   <wire_or_reg> <enb>;                            // Port B RAM Enable, for additional power savings, disable BRAM when not in use
-//   <wire_or_reg> <rsta>;                           // Port A output reset (does not affect memory contents)
-//   <wire_or_reg> <rstb>;                           // Port B output reset (does not affect memory contents)
-//   <wire_or_reg> <regcea>;                         // Port A output register enable
-//   <wire_or_reg> <regceb>;                         // Port B output register enable
-//   wire [(NB_COL*COL_WIDTH)-1:0] <douta>; // Port A RAM output data
-//   wire [(NB_COL*COL_WIDTH)-1:0] <doutb>; // Port B RAM output data
-
-
+// the memory interface, so this module also serves as a convenient boundary.
 
 module ram_array 
   import abr_fpga_pkg::*;
@@ -34,7 +14,7 @@ module ram_array
   output logic [A_DATA_WIDTH-1:0] rdata_a_o,
 
   // Port B: word-read port for ABR side
-  input  logic                    clk_b_i,
+  input  logic                    clk_b_i, // pll
   input  logic [B_ADDR_WIDTH-1:0] addr_b_i,
   input  logic [B_DATA_WIDTH-1:0] wdata_b_i,
   input  logic [  B_WE_WIDTH-1:0] we_b_i,
@@ -43,55 +23,97 @@ module ram_array
 
   timeunit 1ns / 1ps;
 
-  localparam int unsigned RamSelectWidth = $clog2(RAM_COUNT); // Number of bits needed to select among RAM_COUNT parallel RAMs
-  localparam int unsigned RamAddrWidth   = RAM_ADDR_WIDTH - RamSelectWidth; // Address width for each individual RAM, after accounting for the bits used to select among parallel RAMs
+  /* Parameters and types */
 
+  localparam int unsigned RamSelectWidth   = $clog2(RAM_COUNT); // Number of bits needed to select among RAM_COUNT parallel RAMs
+  localparam int unsigned FullRamAddrWidth = RamSelectWidth + RAM_ADDR_WIDTH; // Total address width of the combined RAM array, including bits to select RAM and bits for address within each RAM
+
+  typedef logic [RAM_COUNT-1:0][A_DATA_WIDTH-1:0] ram_data_packed_t; // Packed array type for the data read from each RAM, indexed by RAM number
+ 
   /* Signal declarations */
 
-  logic [  A_DATA_WIDTH-1:0]                   a_rdata_muxed;
-  logic [     RAM_COUNT-1:0][A_DATA_WIDTH-1:0] a_rdata_concat;
+  logic [  A_DATA_WIDTH-1:0]                   ram_a_rdata_muxed;
+  ram_data_packed_t                            a_rdata_concat;
 
-  logic [RamSelectWidth-1:0]                   a_ram_select;
-  logic [  RamAddrWidth-1:0]                   a_ram_addr;
-  logic [     RAM_COUNT-1:0]                   a_ram_we;
-  logic [  A_DATA_WIDTH-1:0]                   a_ram_wdata;
+  logic [RamSelectWidth-1:0]                   ram_a_select;
+  logic [RAM_ADDR_WIDTH-1:0]                   ram_a_addr;
+  logic [     RAM_COUNT-1:0]                   ram_a_we;
+  logic [  A_DATA_WIDTH-1:0]                   ram_a_wdata;
 
-  logic [     RAM_COUNT-1:0][A_DATA_WIDTH-1:0] b_ram_wdata;
+  ram_data_packed_t                            ram_b_wdata;
+  ram_data_packed_t                            ram_b_rdata;
+
+  logic ram_a_en;
+  logic ram_b_en;
 
   /* assignments */
-  assign a_ram_select = addr_a_i[             RamSelectWidth-1:0]; // Select RAM based on address bits above the byte offset
-  assign a_ram_addr   = addr_a_i[RAM_ADDR_WIDTH-1:RamSelectWidth]; // RAM address is the upper bits of the address, excluding the byte offset and RAM select bits
-  assign a_ram_wdata  = wdata_a_i; // wdata can be broadcast to all RAMs since it is gated by the write-enable
+  assign ram_a_select = addr_a_i[               RamSelectWidth-1:0]; // Select RAM based on address bits above the byte offset
+  assign ram_a_addr   = addr_a_i[FullRamAddrWidth-1:RamSelectWidth]; // RAM address is the upper bits of the address, excluding the byte offset and RAM select bits
+  assign ram_a_wdata  = wdata_a_i;                                 // wdata can be broadcast to all RAMs since it is gated by the write-enable
+  assign rdata_a_o    = ram_a_rdata_muxed;                             // Mux the read data from the selected RAM to the output
 
-  assign b_ram_wdata  = wdata_b_i; // Transfer the B-port write data to all RAMs since word-writes are expected
+  assign ram_b_wdata  = ram_data_packed_t'(wdata_b_i); // Transfer the B-port write data to all RAMs since word-writes are expected
+  assign rdata_b_o    = B_DATA_WIDTH'(ram_b_rdata);    // Since all RAMs should have the same data for the B-port, we can take the read data from any RAM (e.g. the first one)
+
+  assign ram_a_en = 1'b1; // Enable RAMs on A-port
+  assign ram_b_en = 1'b1; // Enable RAMs on B-port
 
   /* logic */
 
-  // mux the read data and write enables from the selected RAM to the output
+  // mux the read data and write enables from A interface of the selected RAM to the output
   always_comb begin
     // default assignments
-    rdata_a_o = '0;
-    a_ram_we      = '0;
+    ram_a_rdata_muxed = '0;
+    ram_a_we          = '0;
 
-    unique case (a_ram_select)
+    unique case (ram_a_select)
       0: begin 
-        rdata_a_o   = a_rdata_concat[0];
-        a_ram_we[0] = we_a_i;
+        ram_a_rdata_muxed   = a_rdata_concat[0];
+        ram_a_we[0] = we_a_i;
       end
       1: begin 
-        rdata_a_o = a_rdata_concat[1];
-        a_ram_we[1] = we_a_i;
+        ram_a_rdata_muxed = a_rdata_concat[1];
+        ram_a_we[1] = we_a_i;
       end
       2: begin 
-        rdata_a_o = a_rdata_concat[2];
-        a_ram_we[2] = we_a_i;
+        ram_a_rdata_muxed = a_rdata_concat[2];
+        ram_a_we[2] = we_a_i;
       end
       3: begin 
-        rdata_a_o = a_rdata_concat[3];
-        a_ram_we[3] = we_a_i;
+        ram_a_rdata_muxed = a_rdata_concat[3];
+        ram_a_we[3] = we_a_i;
       end
     endcase
 
   end
+
+  // instantiate RAM_COUNT parallel RAMs, each with its own write enable and 
+  // shared address and data inputs. The read data from each RAM is concatenated into a packed array for muxing to the A-port output.
+  genvar ram_idx;
+  generate
+    for (ram_idx = 0; ram_idx < RAM_COUNT; ram_idx = ram_idx+1) begin: ram_gen
+      
+      // Instantiate RAM_COUNT parallel RAMs
+      amd_tdp_bram #(
+        .NB_COL    ( NB_COL                  ),
+        .COL_WIDTH ( COL_WIDTH               ),
+        .RAM_DEPTH ( RAM_DEPTH               )
+      ) ram_inst (
+        .addr_a_i  ( ram_a_addr              ),
+        .addr_b_i  ( addr_b_i                ),
+        .wdata_a_i ( ram_a_wdata             ),
+        .wdata_b_i ( ram_b_wdata[ram_idx]    ),
+        .clk_a_i   ( clk_a_i                 ),
+        .clk_b_i   ( clk_b_i                 ),
+        .we_a_i    ( ram_a_we[ram_idx]       ),
+        .we_b_i    ( we_b_i[ram_idx]         ),
+        .en_a_i    ( ram_a_en                ),
+        .en_b_i    ( ram_b_en                ),
+        .rdata_a_o ( a_rdata_concat[ram_idx] ),
+        .rdata_b_o ( ram_b_rdata[ram_idx]    )
+      );
+      
+    end
+  endgenerate
 
 endmodule : ram_array
--- fpga/rtl/amd_tdp_bram.sv
+++ fpga/rtl/amd_tdp_bram.sv
@@ -4,107 +4,67 @@
 //  The behavior of this RAM is when data is written, the new memory contents at the write
 //  address are presented on the output port.
 
-  parameter NB_COL = <col>;                       // Specify number of columns (number of bytes)
-  parameter COL_WIDTH = <width>;                  // Specify column width (byte width, typically 8 or 9)
-  parameter RAM_DEPTH = <depth>;                  // Specify RAM depth (number of entries)
-  parameter RAM_PERFORMANCE = "HIGH_PERFORMANCE"; // Select "HIGH_PERFORMANCE" or "LOW_LATENCY" 
-  parameter INIT_FILE = "";                       // Specify name/location of RAM initialization file if using one (leave blank if not)
+module amd_tdp_bram #(
+  parameter NB_COL = 1,      // Specify number of columns (number of bytes)
+  parameter COL_WIDTH = 8,   // Specify column width (byte width, typically 8 or 9)
+  parameter RAM_DEPTH = 512  // Specify RAM depth (number of entries)
+)(
+  input  logic [$clog2(RAM_DEPTH-1)-1:0] addr_a_i,  // Port A address bus, width determined from RAM_DEPTH
+  input  logic [$clog2(RAM_DEPTH-1)-1:0] addr_b_i,  // Port B address bus, width determined from RAM_DEPTH
+  input  logic [ (NB_COL*COL_WIDTH)-1:0] wdata_a_i, // Port A RAM input data
+  input  logic [ (NB_COL*COL_WIDTH)-1:0] wdata_b_i, // Port B RAM input data
+  input  logic                           clk_a_i,   // Port A clock
+  input  logic                           clk_b_i,   // Port B clock
+  input  logic [             NB_COL-1:0] we_a_i,    // Port A write enable
+  input  logic [             NB_COL-1:0] we_b_i,    // Port B write enable
+  input  logic                           en_a_i,    // Port A RAM Enable, for additional power savings, disable BRAM when not in use
+  input  logic                           en_b_i,    // Port B RAM Enable, for additional power savings, disable BRAM when not in use
 
-  <wire_or_reg> [clogb2(RAM_DEPTH-1)-1:0] <addra>;  // Port A address bus, width determined from RAM_DEPTH
-  <wire_or_reg> [clogb2(RAM_DEPTH-1)-1:0] <addrb>;  // Port B address bus, width determined from RAM_DEPTH
-  <wire_or_reg> [(NB_COL*COL_WIDTH)-1:0] <dina>;  // Port A RAM input data
-  <wire_or_reg> [(NB_COL*COL_WIDTH)-1:0] <dinb>;  // Port B RAM input data
-  <wire_or_reg> <clka>;                           // Port A clock
-  <wire_or_reg> <clkb>;                           // Port B clock
-  <wire_or_reg> [NB_COL-1:0] <wea>;               // Port A write enable
-  <wire_or_reg> [NB_COL-1:0] <web>;		  // Port B write enable
-  <wire_or_reg> <ena>;                            // Port A RAM Enable, for additional power savings, disable BRAM when not in use
-  <wire_or_reg> <enb>;                            // Port B RAM Enable, for additional power savings, disable BRAM when not in use
-  <wire_or_reg> <rsta>;                           // Port A output reset (does not affect memory contents)
-  <wire_or_reg> <rstb>;                           // Port B output reset (does not affect memory contents)
-  <wire_or_reg> <regcea>;                         // Port A output register enable
-  <wire_or_reg> <regceb>;                         // Port B output register enable
-  wire [(NB_COL*COL_WIDTH)-1:0] <douta>; // Port A RAM output data
-  wire [(NB_COL*COL_WIDTH)-1:0] <doutb>; // Port B RAM output data
+  output logic [(NB_COL*COL_WIDTH)-1:0] rdata_a_o,  // Port A RAM output data
+  output logic [(NB_COL*COL_WIDTH)-1:0] rdata_b_o   // Port B RAM output data
 
-  reg [(NB_COL*COL_WIDTH)-1:0] <ram_name> [RAM_DEPTH-1:0];
-  reg [(NB_COL*COL_WIDTH)-1:0] <ram_data_a> = {(NB_COL*COL_WIDTH){1'b0}};
-  reg [(NB_COL*COL_WIDTH)-1:0] <ram_data_b> = {(NB_COL*COL_WIDTH){1'b0}};
+);
 
-  // The following code either initializes the memory values to a specified file or to all zeros to match hardware
-  generate
-    if (INIT_FILE != "") begin: use_init_file
-      initial
-        $readmemh(INIT_FILE, <ram_name>, 0, RAM_DEPTH-1);
-    end else begin: init_bram_to_zero
-      integer ram_index;
-      initial
-        for (ram_index = 0; ram_index < RAM_DEPTH; ram_index = ram_index + 1)
-          <ram_name>[ram_index] = {(NB_COL*COL_WIDTH){1'b0}};
+  timeunit 1ns/1ps;
+
+  logic [(NB_COL*COL_WIDTH)-1:0] ram [RAM_DEPTH-1:0];
+
+  logic [(NB_COL*COL_WIDTH)-1:0] ram_rdata_a;
+  logic [(NB_COL*COL_WIDTH)-1:0] ram_rdata_b;
+
+  integer ram_index;
+  
+  // initialise RAM contents to zero
+  initial begin
+    for (ram_index = 0; ram_index < RAM_DEPTH; ram_index = ram_index + 1) begin
+      ram[ram_index] = {(NB_COL*COL_WIDTH){1'b0}};
     end
-  endgenerate
+  end
 
   generate
   genvar i;
      for (i = 0; i < NB_COL; i = i+1) begin: byte_write
-       always @(posedge <clka>)
-         if (<ena>)
-           if (<wea>[i]) begin
-             <ram_name>[<addra>][(i+1)*COL_WIDTH-1:i*COL_WIDTH] <= <dina>[(i+1)*COL_WIDTH-1:i*COL_WIDTH];
-             <ram_data_a>[(i+1)*COL_WIDTH-1:i*COL_WIDTH] <= <dina>[(i+1)*COL_WIDTH-1:i*COL_WIDTH];
+       always @(posedge clk_a_i)
+         if (en_a_i)
+           if (we_a_i[i]) begin
+             ram[addr_a_i][(i+1)*COL_WIDTH-1:i*COL_WIDTH] <= wdata_a_i[(i+1)*COL_WIDTH-1:i*COL_WIDTH];
+             ram_rdata_a[(i+1)*COL_WIDTH-1:i*COL_WIDTH]   <= wdata_a_i[(i+1)*COL_WIDTH-1:i*COL_WIDTH];
            end else begin
-             <ram_data_a>[(i+1)*COL_WIDTH-1:i*COL_WIDTH] <= <ram_name>[<addra>][(i+1)*COL_WIDTH-1:i*COL_WIDTH];
+             ram_rdata_a[(i+1)*COL_WIDTH-1:i*COL_WIDTH]    <= ram[addr_a_i][(i+1)*COL_WIDTH-1:i*COL_WIDTH];
            end
 
-       always @(posedge <clkb>)
-         if (<enb>)
-           if (<web>[i]) begin
-             <ram_name>[<addrb>][(i+1)*COL_WIDTH-1:i*COL_WIDTH] <= <dinb>[(i+1)*COL_WIDTH-1:i*COL_WIDTH];
-             <ram_data_b>[(i+1)*COL_WIDTH-1:i*COL_WIDTH] <= <dinb>[(i+1)*COL_WIDTH-1:i*COL_WIDTH];
+       always @(posedge clk_b_i)
+         if (en_b_i)
+           if (we_b_i[i]) begin
+             ram[addr_b_i][(i+1)*COL_WIDTH-1:i*COL_WIDTH] <= wdata_b_i[(i+1)*COL_WIDTH-1:i*COL_WIDTH];
+             ram_rdata_b[(i+1)*COL_WIDTH-1:i*COL_WIDTH]   <= wdata_b_i[(i+1)*COL_WIDTH-1:i*COL_WIDTH];
            end else begin
-             <ram_data_b>[(i+1)*COL_WIDTH-1:i*COL_WIDTH] <= <ram_name>[addrb][(i+1)*COL_WIDTH-1:i*COL_WIDTH];
+             ram_rdata_b[(i+1)*COL_WIDTH-1:i*COL_WIDTH]   <= ram[addr_b_i][(i+1)*COL_WIDTH-1:i*COL_WIDTH];
            end
      end
   endgenerate
 
-  //  The following code generates HIGH_PERFORMANCE (use output register) or LOW_LATENCY (no output register)
-  generate
-    if (RAM_PERFORMANCE == "LOW_LATENCY") begin: no_output_register
+  assign rdata_a_o = ram_rdata_a;
+  assign rdata_b_o = ram_rdata_b;
 
-      // The following is a 1 clock cycle read latency at the cost of a longer clock-to-out timing
-       assign <douta> = <ram_data_a>;
-       assign <doutb> = <ram_data_b>;
-
-    end else begin: output_register
-
-      // The following is a 2 clock cycle read latency with improve clock-to-out timing
-
-      reg [(NB_COL*COL_WIDTH)-1:0] douta_reg = {(NB_COL*COL_WIDTH){1'b0}};
-      reg [(NB_COL*COL_WIDTH)-1:0] doutb_reg = {(NB_COL*COL_WIDTH){1'b0}};
-
-      always @(posedge <clka>)
-        if (<rsta>)
-          douta_reg <= {(NB_COL*COL_WIDTH){1'b0}};
-        else if (regcea)
-          douta_reg <= <ram_data_a>;
-
-      always @(posedge <clkb>)
-        if (<rstb>)
-          doutb_reg <= {(NB_COL*COL_WIDTH){1'b0}};
-        else if (<regceb>)
-          doutb_reg <= <ram_data_b>;
-
-      assign <douta> = douta_reg;
-      assign <doutb> = doutb_reg;
-
-    end
-  endgenerate
-
-  //  The following function calculates the address width based on specified RAM depth
-  function integer clogb2;
-    input integer depth;
-      for (clogb2=0; depth>0; clogb2=clogb2+1)
-        depth = depth >> 1;
-  endfunction
-							
-						
+endmodule : amd_tdp_bram
